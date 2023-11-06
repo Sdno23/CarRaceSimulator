5617607ed542e1f7ecbbf2b3564d65c06c68ce95
Sdno23 <103979026+Sdno23@users.noreply.github.com>
2023-11-06 02:00:02 -1000

    Update Race.java

--- Race.java
+++ Race.java
@@ -1,5 +1,5 @@
 /*
- * @author XXXXX8
+ * @author XXXXX08
  *
  * @version 1.0 07/26/2020
  *
@@ -10,7 +10,7 @@
  * Lecturer's Name: C. Charters
  */
 
-package CarRaceSimulator;
+package carracesimulator;
 
 import java.io.*;
 import java.util.Scanner;
@@ -18,7 +18,7 @@ import java.io.IOException;
 
 /**
  *
- * @author 3156908
+ * @author XXXXX08
  */
 
 public class Race {
